8bed8750d7fe34f0dbf0cd40c110bd90ac0ba1ea
kad02 <b.kaden.02@gmail.com>
2025-06-04 14:36:54 +0100

    trt effects

--- Functions.R
+++ Functions.R
@@ -1,45 +1,28 @@
-# Simple 2 arm trial
-# Common variance
-# 2 subpopulations
+# Pass treatment effects for subs
+# Variance, total sample size n, and fraction in S1
 
-Trial_Std  <- function(n, lambda, mu_A_S1, mu_A_S2, mu_B_S1, mu_B_S2, sd) {
-  # n: total sample size
-  n1 <- n * lambda
-  n2 <- n * (1 - lambda)
-  
-  # Realisations for subpopulation 1
-  A_S1 <- rnorm(n1/2, mean = mu_A_S1, sd = sd)
-  B_S1 <- rnorm(n1/2, mean = mu_B_S1, sd = sd)
-  
-  # Realisations for subpopulation 2
-  A_S2 <- rnorm(n2/2, mean = mu_A_S2, sd = sd)
-  B_S2 <- rnorm(n2/2, mean = mu_B_S2, sd = sd)
-  
-  return(list(A_S1 = A_S1, B_S1 = B_S1, A_S2 = A_S2, B_S2 = B_S2))
-}
+# Will need to update with tau for multiple stages
 
-
-
-# Calculate trt effects
-calculate_thetas <- function(A_S1, B_S1, A_S2, B_S2, lambda) {
-  # Means for subpopulation 1
-  mu_hat_A_S1 <- mean(A_S1)
-  mu_hat_B_S1 <- mean(B_S1)
-  
-  # Means for subpopulation 2
-  mu_hat_A_S2 <- mean(A_S2)
-  mu_hat_B_S2 <- mean(B_S2)
-  
-  
-  # Treatment effects
-  theta1 <- mu_hat_A_S1 - mu_hat_B_S1
-  theta2 <- mu_hat_A_S2 - mu_hat_B_S2
-  
+treatment_effect <- function(theta1, theta2, sigma, lambda, n) {
+  hat_theta1 <- rnorm(1, mean = theta1, sd = (4 * sigma^2) / (lambda * n))
+  hat_theta2 <- rnorm(1, mean = theta2, sd = (4 * sigma^2) / ((1 - lambda) * n))
   theta3 <- lambda * theta1 + (1 - lambda) * theta2
+  hat_theta3 <- rnorm(1, mean = theta3, sd = (4 * sigma^2) / n)
   
-  return(list(theta1 = theta1, theta2 = theta2, theta3 = theta3))
+  return(list(hat_theta1 = hat_theta1, hat_theta2 = hat_theta2, hat_theta3 = hat_theta3))
 }
 
+
+
+
+
+
+
+
+
+
+
+#############################
 
 # Calculate Z and P values
 calculate_zvals <- function(theta1, theta3, n, lambda, sd) {
